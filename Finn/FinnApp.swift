0f54d5e7adbb8824079284839dba4d67e496359d
Makovey Vladislav <vladislav.makovey@raiffeisen.ru>
2024-06-05 22:04:50 +0300

    initial commit

---
+++ Finn/FinnApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinnApp.swift
+//  Finn
+//
+//  Created by MAKOVEY Vladislav on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FinnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
